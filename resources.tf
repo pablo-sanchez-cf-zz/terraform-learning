a63d36a83b17437bd56cf29508080cdaa85e430c
Pablo Sanchez <psanchez@datapipe.com>
2019-03-22 10:56:05 -0400

    update

--- resources.tf
+++ resources.tf
@@ -1,9 +1,10 @@
-# Create a new VPC
+# Creates a new VPC in GCP.
 resource "google_compute_network" "vpc-test-2" {
   name = "devnetwork"
   auto_create_subnetworks = true
 }
 
+# Creates a new VPC in AWS.
 resource  "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
